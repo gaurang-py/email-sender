a9a390fb0b55e1821d175703e9a73f8a0b37a8cf
gaurang-py <nileshpatel5213@gmail.com>
2024-01-31 12:21:14 +0530

    DONE

--- components/bulkEmailsPage/tabOne.js
+++ components/bulkEmailsPage/tabOne.js
@@ -66,6 +66,11 @@ export default function TabOne() {
     // Set the sanitized HTML content
     setBody(sanitizedContent);
   };
+  const handleContentChange = (event) => {
+    // Update the body state with the current content
+    setBody(event.target.innerHTML);
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
     setError(false);
@@ -230,9 +235,9 @@ export default function TabOne() {
         onPaste={handlePaste}
         className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
         contentEditable
+        onInput={handleContentChange}
+        dangerouslySetInnerHTML={{ __html: body }}
       ></div>
-      <div dangerouslySetInnerHTML={{ __html: body }} />
-
       {/* <textarea
   id="Body"
   rows="6"
